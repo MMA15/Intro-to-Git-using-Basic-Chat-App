da02a6b2ef5043014d41e636bcddc59694d7a0b1
Mubashir Mir <mubashirmir15@gmail.com>
2019-02-13 18:43:56 -0500

    ensure always on last msg by adding Jquery animate and ScrollTop

--- client/app.js
+++ client/app.js
@@ -21,15 +21,19 @@ $('.userform').submit(function(e) {
 $('.messagearea').submit(function(e){
 	e.preventDefault();
 	var text = $('#message').val();
-	socket.emit('message', socket.user.bold() + ' says: '+ text);/*The code above says to emit the textual message to the server instead of performing our temporary alert behaviour.*/
+	socket.emit('message', socket.user + ' says: '+ text);/*The code above says to emit the textual message to the server instead of performing our temporary alert behaviour.*/
 	$('#message').val(''); /* The second line in the code simply clears the input so that another message can be typed by the same user.*/	
 	return false;
 });
 
 socket.on('message', function(msg){
 	$('<li>').text(msg).appendTo('#history');
+	$('#history').animate({scrollTop: $('#history').prop("scrollHeight")}, 500);
+
 });
 
 socket.on('user left', function(number_of_ppl){
 	$('<li>').text("Someone left. There is " + number_of_ppl + " user(s) in the chat.").appendTo('#history');
+	$('#history').animate({scrollTop: $('#history').prop("scrollHeight")}, 500);
+
 });
